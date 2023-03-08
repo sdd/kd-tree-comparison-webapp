d4a4693ce8ef9aaed835c97e04d29811e4ae8890
Scott Donnelly <scott@donnel.ly>
2023-03-08 21:16:30 +0000

    feat: added charts

--- src/App.tsx
+++ src/App.tsx
@@ -3,78 +3,82 @@ import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsiv
 
 import './App.css'
 
+import raw_data from '../all-benchmarks.json'
+import _ from 'lodash';
+
+import { scaleLog } from 'd3-scale';
+const scale = scaleLog().base(Math.E);
+
+const stringToColour = function(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
+}
+
 function App() {
-    const data = [
-        {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
-    ];
+    const data = raw_data.map(d => {
+        const [test, params, node_qty] = d.id.split('/');
+        const [lib, dimensionality, axis_type] = params.split(" ");
+        return {
+            id: d.id,
+            duration: d.mean.estimate / 10e6,
+            test,
+            node_qty,
+            lib,
+            dimensionality,
+            axis_type
+        };
+    });
+
+    const data_by_test = Object.entries(_.groupBy(data, 'test'))
+        .map(([name, data]) => [name, _.groupBy(data, 'node_qty')]);
+
 
     return (
         <div className="App">
             <h1>KD Tree Benchmark</h1>
             <div style={{width:"100%",height:800}}>
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <XAxis dataKey="name"/>
-                    <YAxis/>
-                    <Tooltip/>
-                    <Legend/>
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-                    <Line type="monotone" dataKey="uv" stroke="#82ca9d"/>
-                </LineChart>
+                { data_by_test.map(([name, data]) => (
+                    <div key={name}>
+                        <h2>{name}</h2>
+                        <Chart data={data} />
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
+}
+
+function Chart({data}) {
+    return (
+        <LineChart
+            width={1000}
+            height={300}
+            data={Object.entries(data)}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+        >
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey={x => x[0]}/>
+            <YAxis scale="log" domain={[1, 'dataMax']}/>
+            <Tooltip/>
+            <Legend/>
+            { data['100'].map((datum, idx) => (
+                <Line key={idx} type="monotoneX" name={`${datum.lib} ${datum.dimensionality} ${datum.axis_type}`} dataKey={d => d[1][idx] ? Number(d[1][idx].duration).toPrecision(3) : 0} stroke={stringToColour(`${datum.lib} ${datum.dimensionality} ${datum.axis_type}`)} />
+            ))}
+        </LineChart>
+    );
 }
 
 export default App
